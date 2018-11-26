9e2fbf4a2fc67175a410f52122c44d6151f5ba7d
zyh19941109 <1124596031@qq.com>
2018-11-26 21:53:35 +0800

    裁剪图片根据像素比调节大小，解决图片模糊

--- src/pages/upload/index.js
+++ src/pages/upload/index.js
@@ -2,13 +2,19 @@ import Taro, { Component } from '@tarojs/taro';
 import { View, Canvas, CoverView } from '@tarojs/components';
 import WeCropper from 'we-cropper';
 const device = Taro.getSystemInfoSync();
+const devicePixelRatio = device.pixelRatio;
 const windowWidth = device.windowWidth;
 const windowHeight = device.windowHeight;
 
-let style = {
+let cropper = {
 	width: `${windowWidth}px`,
 	height: `${windowHeight}px`
 };
+
+let target = {
+	width: `${devicePixelRatio * windowWidth}px`,
+	height: `${devicePixelRatio * windowHeight}px`
+}
 
 import './index.less';
 
@@ -32,7 +38,9 @@ class Upload extends Component {
 					height: 300
 				}
 			},
-			wecropper: null
+			wecropper: null,
+			srcUrl: '',
+			targetCtx: null
 		};
 	}
 
@@ -61,7 +69,8 @@ class Upload extends Component {
 			})
 			.updateCanvas();
 		this.setState({
-			wecropper: wecropperObj
+			wecropper: wecropperObj,
+			targetCtx: wx.createCanvasContext('target')
 		});
 	}
 
@@ -81,13 +90,15 @@ class Upload extends Component {
 	uploadTap() {
 		Taro.chooseImage({
 			count: 1, // 默认9
-			sizeType: [ 'original' ], // 可以指定是原图还是压缩图，默认二者都有  sizeType: ['original', 'compressed'],
-			sourceType: [ 'album', 'camera' ], // 可以指定来源是相册还是相机，默认二者都有
-			success: (res) => {
-				const src = res.tempFilePaths[0];
-				// console.log(src)
+			sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有  sizeType: ['original', 'compressed'],
+			sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+			success: res => {
+				this.setState({
+					srcUrl: res.tempFilePaths[0]
+				})
+				// const src = res.tempFilePaths[0];
 				// 获取裁剪图片资源后，给data添加src属性及其值
-				this.state.wecropper.pushOrign(src);
+				// this.state.wecropper.pushOrign(src);
 			}
 		});
 	}
@@ -99,48 +110,111 @@ class Upload extends Component {
 			icon: 'loading',
 			mask: true
 		});
-		this.state.wecropper.getCropperImage((src) => {
-			if (src) {
-				console.log(src);
-				return;
-				Taro.uploadFile({
-					url: util.baseUrl + 'ImageUpload', //上传图片接口
-					filePath: src,
-					name: 'ImageFile',
-					formData: {
-						//参数...
-					},
-					header: util.commonHeader(), //公共header
-					success: (res) => {
-						// console.log(res)
-						let tempFilePaths = JSON.parse(res.data).Body.ImageUrl;
-						Taro.setStorageSync('cutImg', tempFilePaths);
-						setTimeout((_) => {
-							Taro.hideToast();
-							Taro.navigateBack({
-								delta: 1
-							});
-						}, 300);
-					},
-					fail: (err) => {
-						console.log(err, 'fail');
+		let {
+			imgLeft,
+			imgTop,
+			scaleWidth,
+			scaleHeight
+		} = this.state.wecropper; // 获取图片在原画布坐标位置及宽高
+		let {
+			x,
+			y,
+			width,
+			height
+		} = this.state.wecropper.cut; // 获取裁剪框位置及大小
+		// 所有参数乘设备像素比
+		imgLeft = imgLeft * devicePixelRatio;
+		imgTop = imgTop * devicePixelRatio;
+		scaleWidth = scaleWidth * devicePixelRatio;
+		scaleHeight = scaleHeight * devicePixelRatio;
+		x = x * devicePixelRatio;
+		y = y * devicePixelRatio;
+		width = width * devicePixelRatio;
+		height = height * devicePixelRatio;
+		// 这里是目标canvas画布的id值
+		console.log(this.state.srcUrl, imgLeft, imgTop, scaleWidth, scaleHeight, x, y, width, height)
+		this.state.targetCtx.drawImage(this.state.srcUrl, imgLeft, imgTop, scaleWidth, scaleHeight) // 第一个参数代表被裁剪图片的临时路径
+		this.state.targetCtx.draw();
+		setTimeout(_ => {
+			wx.canvasToTempFilePath({
+				canvasId: 'target',
+				x,
+				y,
+				width,
+				height,
+				success: res => {
+					const tmpPath = res.tempFilePath;
+					if (tmpPath) {
+						wx.uploadFile({
+							url: util.baseUrl + 'ImageUpload', //上传图片接口
+							filePath: tmpPath,
+							name: 'ImageFile',
+							formData: {
+								//参数...
+							},
+							header: util.commonHeader(), //公共header
+							success: res => {
+								// console.log(res)
+								let tempFilePaths = JSON.parse(res.data).Body.ImageUrl;
+								wx.setStorageSync('cutImg', tempFilePaths);
+								setTimeout(_ => {
+									wx.hideToast()
+									wx.navigateBack({
+										delta: 1
+									});
+								}, 300)
+							},
+							fail: err => {
+								console.log(err, 'fail')
+							}
+						})
+					} else {
+						console.log('获取图片地址失败，请稍后重试')
 					}
-				});
-			} else {
-				console.log('获取图片地址失败，请稍后重试');
-			}
-		});
+				}
+			})
+		}, 200)
+		// this.state.wecropper.getCropperImage((src) => {
+		// 	if (src) {
+		// 		console.log(src);
+		// 		Taro.uploadFile({
+		// 			url: util.baseUrl + 'ImageUpload', //上传图片接口
+		// 			filePath: src,
+		// 			name: 'ImageFile',
+		// 			formData: {
+		// 				//参数...
+		// 			},
+		// 			header: util.commonHeader(), //公共header
+		// 			success: (res) => {
+		// 				// console.log(res)
+		// 				let tempFilePaths = JSON.parse(res.data).Body.ImageUrl;
+		// 				Taro.setStorageSync('cutImg', tempFilePaths);
+		// 				setTimeout(_ => {
+		// 					Taro.hideToast();
+		// 					Taro.navigateBack({
+		// 						delta: 1
+		// 					});
+		// 				}, 300);
+		// 			},
+		// 			fail: (err) => {
+		// 				console.log(err, 'fail');
+		// 			}
+		// 		});
+		// 	} else {
+		// 		console.log('获取图片地址失败，请稍后重试');
+		// 	}
+		// });
 	}
 
 	componentWillReceiveProps(nextProps) {
 		console.log(this.props, nextProps);
 	}
 
-	componentWillUnmount() {}
+	componentWillUnmount() { }
 
-	componentDidShow() {}
+	componentDidShow() { }
 
-	componentDidHide() {}
+	componentDidHide() { }
 
 	render() {
 		return (
@@ -152,7 +226,12 @@ class Upload extends Component {
 					onTouchStart={this.touchStart}
 					onTouchMove={this.touchMove}
 					onTouchEnd={this.touchEnd}
-					style={style}
+					style={cropper}
+				/>
+				<Canvas
+					className="target"
+					canvas-id="target"
+					style={target}
 				/>
 				<CoverView className="cropper-buttons">
 					<CoverView className="uploadImg" onClick={this.uploadTap}>
