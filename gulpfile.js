54428d56d9e5d43487f0e51a087337b44488f4ff
zyh <1124596031@qq.com>
2019-07-30 18:06:02 +0800

    更新注释

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,8 @@ const gulpZip = () => gulp.src(path.resolve(distFile + '/**'))
 
 //压缩打包文件
 gulp.task('weapp', gulpZip)
+
+// gulp.task--定义任务
+// gulp.src--找到需要执行任务的文件
+// gulp.dest--执行任务的文件的去处
+// gulp.watch--观察文件是否发生变化
