4a29b6ae855a55d03a38d2634d0aea827568eb45
zyh <1124596031@qq.com>
2019-12-16 10:53:58 +0800

    自动删除上次打包zip文件

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,22 @@ const path = require('path');
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const dayjs = require('dayjs');
+const del = require('del');
 const distFile = 'dist'; //打包目录
 const packageInfo = require("./package.json");
 
 let platform = process.argv[process.argv.length - 1] || 'weapp';
 
-const gulpZip = () => gulp.src(path.resolve(distFile + '/**'))
-  .pipe(zip('小程序' + platform + '-' + packageInfo.version + '-' + dayjs().format('YYYY-MM-DD HH-mm-ss') + '.zip'))
-  .pipe(gulp.dest('./'))
+const gulpZip = async () => {
+  const deletedPaths = await del(['*.zip'])
+  console.log(deletedPaths)
+  return gulp.src(path.resolve(distFile + '/**'))
+    .pipe(zip('小程序' + platform + '-' + packageInfo.version + '-' + dayjs().format('YYYY-MM-DD HH-mm-ss') + '.zip'))
+    .pipe(gulp.dest('./'))
+}
 
 //压缩打包文件
-gulp.task('weapp', gulpZip)
+gulp.task(platform, gulpZip)
 
 // gulp.task--定义任务
 // gulp.src--找到需要执行任务的文件
